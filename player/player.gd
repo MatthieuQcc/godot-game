df96f92de3c1534bfcb9da50a48fae9c2b42561b
Matthieu <matthieu.quaccia@gmail.com>
2025-09-23 07:15:15 +0200

    Initial commit du projet Godot

---
+++ player/player.gd
@@ -0,0 +1,213 @@
+extends CharacterBody2D
+
+const WALK_SPEED = 60
+const RUN_SPEED = 120  # vitesse plus rapide quand on court
+const ROLL_SPEED = 125
+
+var input_vector := Vector2.ZERO
+var last_input_vector := Vector2.DOWN
+var is_attacking := false
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _physics_process(_delta: float) -> void:
+	# Si on est en attaque → on bloque les inputs
+	if is_attacking:
+		return
+
+	input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var is_running = Input.is_action_pressed("run")
+	var speed = WALK_SPEED
+	if is_running:
+		speed = RUN_SPEED
+
+	# --- Attaque ---
+	if Input.is_action_just_pressed("attack"):
+		play_attack()
+		return
+
+	# --- Roulade ---
+	if Input.is_action_just_pressed("roll"):
+		play_roll()
+		velocity = last_input_vector * ROLL_SPEED
+		move_and_slide()
+		return
+
+	# --- Déplacement ---
+	if input_vector != Vector2.ZERO:
+		last_input_vector = input_vector
+		velocity = input_vector * speed
+		play_move(input_vector, is_running)
+	else:
+		velocity = Vector2.ZERO
+		play_stand()
+
+	move_and_slide()
+
+
+# -------------------------
+# Animations
+# -------------------------
+
+func play_move(dir: Vector2, is_running: bool):
+	var prefix = "walk_"
+	if is_running:
+		prefix = "run_"
+	if dir.x < 0:
+		sprite.play(prefix + "left")
+	elif dir.x > 0:
+		sprite.play(prefix + "right")
+	elif dir.y < 0:
+		sprite.play(prefix + "up")
+	else:
+		sprite.play(prefix + "down")
+
+
+func play_stand():
+	if last_input_vector.x < 0:
+		sprite.play("stand_left")
+	elif last_input_vector.x > 0:
+		sprite.play("stand_right")
+	elif last_input_vector.y < 0:
+		sprite.play("stand_up")
+	else:
+		sprite.play("stand_down")
+
+
+func play_attack():
+	is_attacking = true
+	if last_input_vector.x < 0:
+		sprite.play("attack_left")
+	elif last_input_vector.x > 0:
+		sprite.play("attack_right")
+	elif last_input_vector.y < 0:
+		sprite.play("attack_up")
+	else:
+		sprite.play("attack_down")
+
+
+func play_roll():
+	if last_input_vector.x < 0:
+		sprite.play("roll_left")
+	elif last_input_vector.x > 0:
+		sprite.play("roll_right")
+	elif last_input_vector.y < 0:
+		sprite.play("roll_up")
+	else:
+		sprite.play("roll_down")
+
+
+# -------------------------
+# Callback fin d'anim
+# -------------------------
+
+func _on_animated_sprite_2d_animation_finished():
+	if sprite.animation.begins_with("attack"):
+		is_attacking = false
+		play_stand()
+
+
+
+
+
+#extends CharacterBody2D
+#
+#const WALK_SPEED = 60
+#const ROLL_SPEED = 125
+#
+#var input_vector := Vector2.ZERO
+#var last_input_vector := Vector2.DOWN
+#var is_attacking := false
+#
+#@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+#
+#
+#func _physics_process(delta: float) -> void:
+	## Si on est en attaque → on bloque les inputs
+	#if is_attacking:
+		#return
+#
+	#input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+#
+	## --- Attaque ---
+	#if Input.is_action_just_pressed("attack"):
+		#play_attack()
+		#return
+#
+	## --- Roulade ---
+	#if Input.is_action_just_pressed("roll"):
+		#play_roll()
+		#velocity = last_input_vector * ROLL_SPEED
+		#move_and_slide()
+		#return
+#
+	## --- Déplacement ---
+	#if input_vector != Vector2.ZERO:
+		#last_input_vector = input_vector
+		#velocity = input_vector * WALK_SPEED
+		#play_run(input_vector)
+	#else:
+		#velocity = Vector2.ZERO
+		#play_stand()
+#
+	#move_and_slide()
+#
+#
+## -------------------------
+## Animations
+## -------------------------
+#
+#func play_run(dir: Vector2):
+	#if dir.x < 0:
+		#sprite.play("run_left")
+	#elif dir.x > 0:
+		#sprite.play("run_right")
+	#elif dir.y < 0:
+		#sprite.play("run_up")
+	#else:
+		#sprite.play("run_down")
+#
+#
+#func play_stand():
+	#if last_input_vector.x < 0:
+		#sprite.play("stand_left")
+	#elif last_input_vector.x > 0:
+		#sprite.play("stand_right")
+	#elif last_input_vector.y < 0:
+		#sprite.play("stand_up")
+	#else:
+		#sprite.play("stand_down")
+#
+#
+#func play_attack():
+	#is_attacking = true
+	#if last_input_vector.x < 0:
+		#sprite.play("attack_left")
+	#elif last_input_vector.x > 0:
+		#sprite.play("attack_right")
+	#elif last_input_vector.y < 0:
+		#sprite.play("attack_up")
+	#else:
+		#sprite.play("attack_down")
+#
+#
+#func play_roll():
+	#if last_input_vector.x < 0:
+		#sprite.play("roll_left")
+	#elif last_input_vector.x > 0:
+		#sprite.play("roll_right")
+	#elif last_input_vector.y < 0:
+		#sprite.play("roll_up")
+	#else:
+		#sprite.play("roll_down")
+#
+#
+## -------------------------
+## Callback fin d'anim
+## -------------------------
+#
+#func _on_animated_sprite_2d_animation_finished():
+	#if sprite.animation.begins_with("attack"):
+		#is_attacking = false
+		#play_stand()
